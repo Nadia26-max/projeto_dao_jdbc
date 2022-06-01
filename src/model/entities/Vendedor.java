d48041c07b75acdbabc090a5f4f875c3cd7380fc
Nadia Araujo <nadiaaraujo1@gmail.com>
2022-06-01 20:40:06 -0300

    Classe de Entidade de Vendedor

---
+++ src/model/entities/Vendedor.java
@@ -0,0 +1,105 @@
+package model.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+public class Vendedor implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	private String nome;
+	private String email;
+	private Date aniversario;
+	private Double baseSalario;
+	
+	//Associação - Vendedor tem 1 Departamento
+	private Departamento departament;
+	
+	public Vendedor() {
+	}
+	
+	public Vendedor(Integer id, String nome, String email, Date aniversario, 
+			Double baseSalario, Departamento departament) {
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+		this.aniversario = aniversario;
+		this.baseSalario = baseSalario;
+		this.departament = departament;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getAniversario() {
+		return aniversario;
+	}
+
+	public void setAniversario(Date aniversario) {
+		this.aniversario = aniversario;
+	}
+
+	public Double getBaseSalario() {
+		return baseSalario;
+	}
+
+	public void setBaseSalario(Double baseSalario) {
+		this.baseSalario = baseSalario;
+	}
+
+	public Departamento getDepartament() {
+		return departament;
+	}
+
+	public void setDepartament(Departamento departament) {
+		this.departament = departament;
+	}
+
+	//Retorna o id (o hash retorna numero inteiro)
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Vendedor other = (Vendedor) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return "Vendedor: id = " + id + " -- nome=" + nome + " -- email = " + email 
+				+ " -- aniversario = " + aniversario + " -- baseSalario = " + baseSalario 
+				+ " -- departament = " + departament;
+	}
+}
