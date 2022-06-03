c4dfde67d1268e8f27ad633e12908ed5ac35ddd6
Nadia Araujo <nadiaaraujo1@gmail.com>
2022-06-03 20:57:03 -0300

    Implementação do delete

--- src/application/Programa.java
+++ src/application/Programa.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
+
 import model.dao.DaoFabrica;
 import model.dao.VendedorDao;
 import model.entities.Departamento;
@@ -16,7 +18,7 @@ public class Programa {
 		System.out.println("##Teste 1: Vendedor findById##");
 		Vendedor vendedor = vendedorDao.findById(3);
 		System.out.println(vendedor);
-
+	/*----------------------------------------------------------------------------*/
 		System.out.println("\n##Teste 2: Vendedor findByDepartamento##");
 
 		Departamento departamento = new Departamento(2, null);
@@ -34,24 +36,37 @@ public class Programa {
 			 * ven.getDepartament());
 			 */
 		}
-
+	/*----------------------------------------------------------------------------*/
 		System.out.println("\n##Teste 3: Vendedor findAll##");
 		list1 = vendedorDao.findAll();//Usando a mesma lista nao precisa ter a declaração da lista antes
 		
 		for (Vendedor ven : list1) {
 			System.out.println(ven);
 		}
-		
+	/*----------------------------------------------------------------------------*/
 		System.out.println("\n##Teste 4: Vendedor Insert##");
 		Vendedor novoVendedor = new Vendedor(null,"Greg","greg@gmail.com",new Date(),4000.0,departamento);
 		vendedorDao.insert(novoVendedor);
 		
 		System.out.println("Inserido! Novo id: " +novoVendedor.getId());//Para verificar se trouxe o id
-	
+	/*----------------------------------------------------------------------------*/
 		System.out.println("\n##Teste 5: Vendedor Update##");
 		vendedor = vendedorDao.findById(1);//Carrego os dados do vendedor com id = 1 no objeto vendedor
 		vendedor.setNome("Martha Wayne");
 		vendedorDao.update(vendedor);
 		System.out.println("Atualização completa");
+	/*----------------------------------------------------------------------------*/
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("\n##Teste 6: Vendedor Delete##");
+		
+		System.out.printf("Informe o identificador que será excluído: ");
+		int id = sc.nextInt();
+		
+		vendedorDao.deleteById(id);
+		
+		System.out.println("Exclusão completa");
+		
+		sc.close();
 	}
 }
--- src/model/impl/VendedorDaoJDBC.java
+++ src/model/impl/VendedorDaoJDBC.java
@@ -15,11 +15,11 @@ import model.dao.VendedorDao;
 import model.entities.Departamento;
 import model.entities.Vendedor;
 
-public class VendedorDaoJDBC implements VendedorDao{
-	
-	//Instanciado antes da operações e assim, usarei em minhas conexoes abaixo
+public class VendedorDaoJDBC implements VendedorDao {
+
+	// Instanciado antes da operações e assim, usarei em minhas conexoes abaixo
 	private Connection con;
-	
+
 	public VendedorDaoJDBC(Connection con) {
 		this.con = con;
 	}
@@ -27,39 +27,34 @@ public class VendedorDaoJDBC implements VendedorDao{
 	@Override
 	public void insert(Vendedor ven) {
 		PreparedStatement st = null;
-		
+
 		try {
-			st = con.prepareStatement(
-					"INSERT INTO vendedor (Name, Email, BirthDate, BaseSalary, DepartmentId)"
-				  + "VALUES (?, ?, ?, ?, ?)",
-				   Statement.RETURN_GENERATED_KEYS);
-			
+			st = con.prepareStatement("INSERT INTO vendedor (Name, Email, BirthDate, BaseSalary, DepartmentId)"
+					+ "VALUES (?, ?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
+
 			st.setString(1, ven.getNome());
 			st.setString(2, ven.getEmail());
 			st.setDate(3, new java.sql.Date(ven.getAniversario().getTime()));
 			st.setDouble(4, ven.getBaseSalario());
 			st.setInt(5, ven.getDepartament().getId());
-			
+
 			int linhasAfetadas = st.executeUpdate();
-			
-			if(linhasAfetadas > 0) {//Se Inserir
+
+			if (linhasAfetadas > 0) {// Se Inserir
 				ResultSet rs = st.getGeneratedKeys();
-				if(rs.next()) { //Se Inserir - 1 dado
-					int id = rs.getInt(1);//Primeira posição das chaves
-					
-					ven.setId(id);//ven ja recebe o valor da variavel id
+				if (rs.next()) { // Se Inserir - 1 dado
+					int id = rs.getInt(1);// Primeira posição das chaves
+
+					ven.setId(id);// ven ja recebe o valor da variavel id
 				}
-				
+
 				DB.fechaResultSet(rs);
-			}
-			else {//Caso nenhuma linha seja alterada
+			} else {// Caso nenhuma linha seja alterada
 				throw new DbExcecao("Erro inesperado!Nenhuma linha afetada");
 			}
-		}
-		catch (SQLException e) {
+		} catch (SQLException e) {
 			throw new DbExcecao(e.getMessage());
-		}
-		finally {
+		} finally {
 			DB.fechaStatement(st);
 		}
 	}
@@ -67,25 +62,25 @@ public class VendedorDaoJDBC implements VendedorDao{
 	@Override
 	public void update(Vendedor ven) {
 		PreparedStatement st = null;
-		
+
 		try {
-			st = con.prepareStatement(
-				"UPDATE vendedor "
-			  + "SET Name = ?, Email = ?, BirthDate = ?, BaseSalary = ?, DepartmentId = ? "
-			  + "WHERE Id = ?");
-			
+			st = con.prepareStatement("UPDATE vendedor "
+					+ "SET Name = ?, Email = ?, BirthDate = ?, BaseSalary = ?, DepartmentId = ? " + "WHERE Id = ?");
+
 			st.setString(1, ven.getNome());
 			st.setString(2, ven.getEmail());
 			st.setDate(3, new java.sql.Date(ven.getAniversario().getTime()));
 			st.setDouble(4, ven.getBaseSalario());
 			st.setInt(5, ven.getDepartament().getId());
-			st.setInt(6, ven.getId());//id do vendedor - Por causa do where no upadate
-			
+			st.setInt(6, ven.getId());// id do vendedor - Por causa do where no upadate
+
 			st.executeUpdate();
 		}
+
 		catch (SQLException e) {
 			throw new DbExcecao(e.getMessage());
 		}
+
 		finally {
 			DB.fechaStatement(st);
 		}
@@ -93,37 +88,57 @@ public class VendedorDaoJDBC implements VendedorDao{
 
 	@Override
 	public void deleteById(Integer id) {
-		
+
+		PreparedStatement st = null;
+		try {
+			st = con.prepareStatement("DELETE FROM vendedor WHERE Id = ?");
+
+			st.setInt(1, id);// Por causa do where
+				
+			st.executeUpdate();
+			
+			/* Para exibir uma exceção caso id nao exista
+			 
+			int linhasAfetadas = st.executeUpdate();
+			
+			if(linhasAfetadas == 0) {
+				throw new DbExcecao("Nenhum dado foi alterado/excluído");
+				//System.out.println("Nenhum dado foi alterado/excluído");
+			}*/
+			
+		} catch (SQLException e) {
+			throw new DbExcecao(e.getMessage());
+		}
+
+		finally {
+			DB.fechaStatement(st);
+		}
 	}
 
 	@Override
 	public Vendedor findById(Integer id) {
 		PreparedStatement st = null;
 		ResultSet rs = null;
-		
+
 		try {
-			st = con.prepareStatement(
-					"SELECT vendedor.*,departmento.Name as DepName "
-					+ "FROM vendedor "
-					+ "INNER JOIN departmento ON vendedor.DepartmentId = departmento.Id "
-					+ "WHERE vendedor.Id = ?");
+			st = con.prepareStatement("SELECT vendedor.*,departmento.Name as DepName " + "FROM vendedor "
+					+ "INNER JOIN departmento ON vendedor.DepartmentId = departmento.Id " + "WHERE vendedor.Id = ?");
 
 			st.setInt(1, id);
 			rs = st.executeQuery();
-			
-			//Verifica se tem um vendedor pelo id, senao retorna nulo 
-			if(rs.next()) {
+
+			// Verifica se tem um vendedor pelo id, senao retorna nulo
+			if (rs.next()) {
 				Departamento dep = instanciandoDepartamento(rs);
-				
-				Vendedor ven = instanciandoDepartamento(rs,dep);
+
+				Vendedor ven = instanciandoDepartamento(rs, dep);
 				return ven;
 			}
 			return null;
-		}
-		catch (SQLException e) {
+		} catch (SQLException e) {
 			throw new DbExcecao(e.getMessage());
 		}
-		
+
 		finally {
 			DB.fechaStatement(st);
 			DB.fechaResultSet(rs);
@@ -132,66 +147,63 @@ public class VendedorDaoJDBC implements VendedorDao{
 
 	private Vendedor instanciandoDepartamento(ResultSet rs, Departamento dep) throws SQLException {
 		Vendedor ven = new Vendedor();
-		ven.setId(rs.getInt("Id"));//Nome da coluna no bd
+		ven.setId(rs.getInt("Id"));// Nome da coluna no bd
 		ven.setNome(rs.getString("Name"));
 		ven.setEmail(rs.getString("Email"));
 		ven.setAniversario(rs.getDate("BirthDate"));
 		ven.setBaseSalario(rs.getDouble("BaseSalary"));
-		ven.setDepartament(dep);//Referenciando o Departmamento
+		ven.setDepartament(dep);// Referenciando o Departmamento
 		return ven;
 	}
 
 	private Departamento instanciandoDepartamento(ResultSet rs) throws SQLException {
 		Departamento dep = new Departamento();
-		dep.setId(rs.getInt("DepartmentId"));//Nome da coluna no bd
-		dep.setNome(rs.getString("DepName"));//Nome da coluna no bd
+		dep.setId(rs.getInt("DepartmentId"));// Nome da coluna no bd
+		dep.setNome(rs.getString("DepName"));// Nome da coluna no bd
 		return dep;
 	}
 
 	@Override
-	public List<Vendedor> findAll() {//Vai buscar todos os vendedores, ordenar mas nao tem restrição (where)
-		
+	public List<Vendedor> findAll() {// Vai buscar todos os vendedores, ordenar mas nao tem restrição (where)
+
 		PreparedStatement st = null;
 		ResultSet rs = null;
-		
+
 		try {
-			st = con.prepareStatement(
-					"SELECT vendedor.*,departmento.Name as DepName "
-					+ "FROM vendedor "
-					+ "INNER JOIN departmento ON vendedor.DepartmentId = departmento.Id "
-					+ "ORDER BY Name"
-					);
+			st = con.prepareStatement("SELECT vendedor.*,departmento.Name as DepName " + "FROM vendedor "
+					+ "INNER JOIN departmento ON vendedor.DepartmentId = departmento.Id " + "ORDER BY Name");
 
 			rs = st.executeQuery();
-			
-			List<Vendedor> lista = new ArrayList<Vendedor>();
-			
-			//Para nao repetir o Departamento no while (os vendedores que apontam para o departamento e nao o contrario - dois para um)
-			Map<Integer, Departamento> mapa = new HashMap<>();
-			//Criei um mapa vazio e posso guardar qualquer departamento que eu instanciar
 
-			//Portanto, cada vez que passar no while, verifico se ja existe aquele Departamento
-			while(rs.next()) {
-				//Busco dentro do mapa se ja existe alguma id 2, por exemplo. 
+			List<Vendedor> lista = new ArrayList<Vendedor>();
+
+			// Para nao repetir o Departamento no while (os vendedores que apontam para o
+			// departamento e nao o contrario - dois para um)
+			Map<Integer, Departamento> mapa = new HashMap<>();
+			// Criei um mapa vazio e posso guardar qualquer departamento que eu instanciar
+
+			// Portanto, cada vez que passar no while, verifico se ja existe aquele
+			// Departamento
+			while (rs.next()) {
+				// Busco dentro do mapa se ja existe alguma id 2, por exemplo.
 				Departamento dep = mapa.get(rs.getInt("DepartmentId"));
-				
-				//Se nao existir, retorna nulo e instancia o departamento
-				if(dep == null) {
+
+				// Se nao existir, retorna nulo e instancia o departamento
+				if (dep == null) {
 					dep = instanciandoDepartamento(rs);
-					
-					//Guardando o Departamento no map - chave, departamento que vou salvar
-					mapa.put(rs.getInt("DepartmentId"),dep);
-				}			
-				//Apontando para o departamento existente ou o novo que instanciei
-				Vendedor ven = instanciandoDepartamento(rs,dep);
+
+					// Guardando o Departamento no map - chave, departamento que vou salvar
+					mapa.put(rs.getInt("DepartmentId"), dep);
+				}
+				// Apontando para o departamento existente ou o novo que instanciei
+				Vendedor ven = instanciandoDepartamento(rs, dep);
 				lista.add(ven);
 			}
 			return lista;
-		}
-		catch (SQLException e) {
+		} catch (SQLException e) {
 			throw new DbExcecao(e.getMessage());
 		}
-		
+
 		finally {
 			DB.fechaStatement(st);
 			DB.fechaResultSet(rs);
@@ -202,46 +214,44 @@ public class VendedorDaoJDBC implements VendedorDao{
 	public List<Vendedor> findByDepartamento(Departamento departamento) {
 		PreparedStatement st = null;
 		ResultSet rs = null;
-		
+
 		try {
-			st = con.prepareStatement(
-					"SELECT vendedor.*,departmento.Name as DepName "
-					+ "FROM vendedor "
+			st = con.prepareStatement("SELECT vendedor.*,departmento.Name as DepName " + "FROM vendedor "
 					+ "INNER JOIN departmento ON vendedor.DepartmentId = departmento.Id "
-					+ "WHERE DepartmentId = ? ORDER BY Name"
-					);
+					+ "WHERE DepartmentId = ? ORDER BY Name");
 
 			st.setInt(1, departamento.getId());
 			rs = st.executeQuery();
-			
-			List<Vendedor> lista = new ArrayList<Vendedor>();
-			
-			//Para nao repetir o Departamento no while (os vendedores que apontam para o departamento e nao o contrario - dois para um)
-			Map<Integer, Departamento> mapa = new HashMap<>();
-			//Criei um mapa vazio e posso guardar qualquer departamento que eu instanciar
 
-			//Portanto, cada vez que passar no while, verifico se ja existe aquele Departamento
-			while(rs.next()) {
-				//Busco dentro do mapa se ja existe alguma id 2, por exemplo. 
+			List<Vendedor> lista = new ArrayList<Vendedor>();
+
+			// Para nao repetir o Departamento no while (os vendedores que apontam para o
+			// departamento e nao o contrario - dois para um)
+			Map<Integer, Departamento> mapa = new HashMap<>();
+			// Criei um mapa vazio e posso guardar qualquer departamento que eu instanciar
+
+			// Portanto, cada vez que passar no while, verifico se ja existe aquele
+			// Departamento
+			while (rs.next()) {
+				// Busco dentro do mapa se ja existe alguma id 2, por exemplo.
 				Departamento dep = mapa.get(rs.getInt("DepartmentId"));
-				
-				//Se nao existir, retorna nulo e instancia o departamento
-				if(dep == null) {
+
+				// Se nao existir, retorna nulo e instancia o departamento
+				if (dep == null) {
 					dep = instanciandoDepartamento(rs);
-					
-					//Guardando o Departamento no map - chave, departamento que vou salvar
-					mapa.put(rs.getInt("DepartmentId"),dep);
-				}			
-				//Apontando para o departamento existente ou o novo que instanciei
-				Vendedor ven = instanciandoDepartamento(rs,dep);
+
+					// Guardando o Departamento no map - chave, departamento que vou salvar
+					mapa.put(rs.getInt("DepartmentId"), dep);
+				}
+				// Apontando para o departamento existente ou o novo que instanciei
+				Vendedor ven = instanciandoDepartamento(rs, dep);
 				lista.add(ven);
 			}
 			return lista;
-		}
-		catch (SQLException e) {
+		} catch (SQLException e) {
 			throw new DbExcecao(e.getMessage());
 		}
-		
+
 		finally {
 			DB.fechaStatement(st);
 			DB.fechaResultSet(rs);
